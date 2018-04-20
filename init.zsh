018fa4864eb5c825b1eefb4310d1929ac2b44112
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-19 23:15:23 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,11 @@
+p6df::modules::shell::version() { echo "0.0.1" }
+p6df::modules::shell::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::shell::external() { 
+}
+
+p6df::modules::shell::init() {
+}
+
