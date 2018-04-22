ee86e3b56a97b27c24445799e0c51bf49358097c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:35:50 -0400

    brew

--- init.zsh
+++ init.zsh
@@ -3,9 +3,40 @@ p6df::modules::shell::deps()    {
 	ModuleDeps=()
 }
 
-p6df::modules::shell::external() { 
+p6df::modules::shell::external::brew() {
+
+  brew tap sbdchd/skim
+  brew install skim
+
+  brew install aspell
+  brew install coreutils
+  brew install curl
+  brew install jq
+  brew install screen
+  brew install tmux
+  brew install tree
+  brew install wget 
+  brew install xz
+
+  brew install irssi
+  brew install youtube-dl
+}
+
+irc () {
+  if ! irc_attach ; then
+    irc_init
+  fi
 }
 
 p6df::modules::shell::init() {
+
+  alias irc_attach='tmux attach -t irc'
+  alias irc_init='tmux new -s irc irssi'
+
+  export LSCOLORS=Gxfxcxdxbxegedabagacad
+  case "$OSTYPE" in
+        freebsd*|darwin*) alias ll='ls -alFGTh' ;;
+                       *) alias ll='/bin/ls -alFh --color=auto' ;;
+  esac
 }
 
