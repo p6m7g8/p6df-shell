f41d1d5f711bb5553483f729f436c96ab062ea9c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-27 21:05:54 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 p6df::modules::shell::version() { echo "0.0.1" }
-p6df::modules::shell::deps()    { 
+p6df::modules::shell::deps()    {
 	ModuleDeps=(robbyrussell/oh-my-zsh:plugins/encode64)
 }
 
@@ -15,7 +15,7 @@ p6df::modules::shell::external::brew() {
   brew install screen
   brew install tmux
   brew install tree
-  brew install wget 
+  brew install wget
   brew install xz
 
   brew install irssi
@@ -61,10 +61,12 @@ p6df::modules::shell::init() {
   alias irc_attach='tmux attach -t irc'
   alias irc_init='tmux new -s irc irssi'
 
+  alias xclean p6_xclean
+
   export LSCOLORS=Gxfxcxdxbxegedabagacad
   case "$OSTYPE" in
-        freebsd*|darwin*) alias ll='ls -alFGTh' ;;
-                       *) alias ll='/bin/ls -alFh --color=auto' ;;
+	freebsd*|darwin*) alias ll='ls -alFGTh' ;;
+		       *) alias ll='/bin/ls -alFh --color=auto' ;;
   esac
 }
 
@@ -98,23 +100,17 @@ env_version() {
     local ver
 
     if command -v $cmd > /dev/null; then
-        case $prefix in
-            go)
-                ver="$($cmd version 2>/dev/null)"
-                [ -n "$ver" ] || ver=system
-                ;;
-            *) ver="$($cmd version-name 2>/dev/null)" ;;
-        esac
+        ver="$($cmd version-name 2>/dev/null)"
 
-        local v=$(echo $ver | sed -e "s,$prefix,," -e 's,^-,,')
+	local v=$(echo $ver | sed -e "s,$prefix,," -e 's,^-,,')
 
-        if [ x"$v" = x"system" ]; then
-            system_version "$prefix"
-        else
-            echo "${red}$v${magenta}"
-        fi
+	if [ x"$v" = x"system" ]; then
+	    system_version "$prefix"
+	else
+	    echo "$v"
+	fi
     else
-        system_version "$prefix"
+	system_version "$prefix"
     fi
 }
 
@@ -132,8 +128,8 @@ cmd_2_envprefix() {
       scala)    prefix=scala ;;
       lua)      prefix=lua   ;;
     esac
-    
-    echo $prefix    
+
+    echo $prefix
 }
 
 envprefix_2_cmd() {
@@ -141,14 +137,14 @@ envprefix_2_cmd() {
 
     local rcmd
     case $prefix in
-        py)    rcmd=python ;;
-        rb)    rcmd=ruby   ;;
-        pl)    rcmd=perl   ;;
-        go)    rcmd=go     ;;
-        j)     rcmd=java   ;;
-        R)     rcmd=R      ;;
-        scala) rcmd=scala  ;;
-        lua)   rcmd=lua    ;;
+	py)    rcmd=python ;;
+	rb)    rcmd=ruby   ;;
+	pl)    rcmd=perl   ;;
+	go)    rcmd=go     ;;
+	j)     rcmd=java   ;;
+	R)     rcmd=R      ;;
+	scala) rcmd=scala  ;;
+	lua)   rcmd=lua    ;;
     esac
 
     echo $rcmd
@@ -160,20 +156,20 @@ system_version() {
     local rcmd=$(envprefix_2_cmd "$prefix")
 
     if command -v $rcmd > /dev/null; then
-        local ver
-        case $prefix in
-            py)    ver=$($rcmd -V 2>&1 | awk '{print $2}') ;;
-            rb)    ver=$($rcmd -v | awk '{print $2}')      ;;
-            pl)    ver=$($rcmd -v | sed -e 's,.*(,,' -e 's,).*,,' | grep ^v5 | sed -e 's,^v,,') ;;
-            go)    ver=$($rcmd version | awk '{print $3}' | sed -e 's,^go,,') ;;
-            j)     ver=$($rcmd -version 2>&1 | grep Environment | sed -e 's,.*(build ,,' -e 's,).*,,') ;;
-            R)     ver=$($rcmd --version | awk '/ version / { print $3}') ;;
-            scala) ver=$($rcmd -nc -version 2>&1 | awk '{print $5}') ;;
-            lua)   ver=$($rcmd -v | awk '{print $2}') ;;
-        esac
-        echo "${cyan}sys@$ver${magenta}"
+	local ver
+	case $prefix in
+	    py)    ver=$($rcmd -V 2>&1 | awk '{print $2}') ;;
+	    rb)    ver=$($rcmd -v | awk '{print $2}')      ;;
+	    pl)    ver=$($rcmd -v | sed -e 's,.*(,,' -e 's,).*,,' | grep ^v5 | sed -e 's,^v,,') ;;
+	    go)    ver=$($rcmd version | awk '{print $3}' | sed -e 's,^go,,') ;;
+	    j)     ver=$($rcmd -version 2>&1 | grep Environment | sed -e 's,.*(build ,,' -e 's,).*,,') ;;
+	    R)     ver=$($rcmd --version | awk '/ version / { print $3}') ;;
+	    scala) ver=$($rcmd -nc -version 2>&1 | awk '{print $5}') ;;
+	    lua)   ver=$($rcmd -v | awk '{print $2}') ;;
+	esac
+	echo "sys@$ver"
     else
-        echo "${norm}no${magenta}"
+	echo "no"
     fi
 }
 
@@ -187,4 +183,5 @@ now() {
 
     date "+%s"
 }
+
 p6df::modules::shell::init
