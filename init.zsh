63611dccfdf02605f2f7ab82b02064c429d6ccd8
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-27 21:52:57 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -24,6 +24,11 @@ p6df::modules::shell::external::brew() {
 
 p6df::modules::shell::init() {
 
+  p6df::modules::shell::aliases::init
+}
+
+p6df::modules::shell::aliases::init() {
+
   alias '_'='sudo'
   alias rmrf='rm -rf'
   alias cpr='cp -R'
@@ -58,10 +63,12 @@ p6df::modules::shell::init() {
   alias -g ng='| grep -v "\.git"'
   alias -g n="| grep $NAME"
 
-  alias irc_attach='tmux attach -t irc'
-  alias irc_init='tmux new -s irc irssi'
+  alias xclean='p6_xclean'
+  alias proxy_off='p6df::modules::shell::proxy::off'
 
-  alias xclean p6_xclean
+  alias irc='p6df::modules::shell::irc'
+  alias irc_attach='p6df::modules::shell::irc::attach'
+  alias irc_init='p6df::modules::shell::irc::init'
 
   export LSCOLORS=Gxfxcxdxbxegedabagacad
   case "$OSTYPE" in
@@ -70,14 +77,30 @@ p6df::modules::shell::init() {
   esac
 }
 
-p6df::prompt::proxy::line() {
+p6df::modules::shell::irc::attach() {
+  
+  tmux attach -t irc
+}
 
-  if [ -n "${ALL_PROXY}" ]; then
-      echo "${magenta}proxy: [ALL_PROXY=$ALL_PROXY]${norm}"
+p6df::modules::shell::irc::init() {
+
+  tmux new -s irc irssi
+}
+
+p6df::modules::shell::irc() {
+  if ! irc_attach ; then
+    irc_init
   fi
 }
 
-proxy_off() {
+p6df::prompt::proxy::line() {
+
+  if [ -n "${ALL_PROXY}" ]; then
+      echo "proxy:\tALL_PROXY=$ALL_PROXY"
+  fi
+}
+
+p6df::modules::shell::proxy::off() {
 
   local ev
   for ev in `env |grep -i ^proxy`; do
@@ -86,102 +109,3 @@ proxy_off() {
     unset $e
   done
 }
-
-irc () {
-  if ! irc_attach ; then
-    irc_init
-  fi
-}
-
-env_version() {
-    local prefix="$1"
-
-    local cmd="${prefix}env"
-    local ver
-
-    if command -v $cmd > /dev/null; then
-        ver="$($cmd version-name 2>/dev/null)"
-
-	local v=$(echo $ver | sed -e "s,$prefix,," -e 's,^-,,')
-
-	if [ x"$v" = x"system" ]; then
-	    system_version "$prefix"
-	else
-	    echo "$v"
-	fi
-    else
-	system_version "$prefix"
-    fi
-}
-
-cmd_2_envprefix() {
-    local cmd="$1"
-
-    local prefix
-    case $cmd in
-      python)	prefix=py    ;;
-      ruby)     prefix=rb    ;;
-      perl)     prefix=pl    ;;
-      go)       prefix=go    ;;
-      java)     prefix=j     ;;
-      R)        prefix=R     ;;
-      scala)    prefix=scala ;;
-      lua)      prefix=lua   ;;
-    esac
-
-    echo $prefix
-}
-
-envprefix_2_cmd() {
-    local prefix="$1"
-
-    local rcmd
-    case $prefix in
-	py)    rcmd=python ;;
-	rb)    rcmd=ruby   ;;
-	pl)    rcmd=perl   ;;
-	go)    rcmd=go     ;;
-	j)     rcmd=java   ;;
-	R)     rcmd=R      ;;
-	scala) rcmd=scala  ;;
-	lua)   rcmd=lua    ;;
-    esac
-
-    echo $rcmd
-}
-
-system_version() {
-    local prefix="$1"
-
-    local rcmd=$(envprefix_2_cmd "$prefix")
-
-    if command -v $rcmd > /dev/null; then
-	local ver
-	case $prefix in
-	    py)    ver=$($rcmd -V 2>&1 | awk '{print $2}') ;;
-	    rb)    ver=$($rcmd -v | awk '{print $2}')      ;;
-	    pl)    ver=$($rcmd -v | sed -e 's,.*(,,' -e 's,).*,,' | grep ^v5 | sed -e 's,^v,,') ;;
-	    go)    ver=$($rcmd version | awk '{print $3}' | sed -e 's,^go,,') ;;
-	    j)     ver=$($rcmd -version 2>&1 | grep Environment | sed -e 's,.*(build ,,' -e 's,).*,,') ;;
-	    R)     ver=$($rcmd --version | awk '/ version / { print $3}') ;;
-	    scala) ver=$($rcmd -nc -version 2>&1 | awk '{print $5}') ;;
-	    lua)   ver=$($rcmd -v | awk '{print $2}') ;;
-	esac
-	echo "sys@$ver"
-    else
-	echo "no"
-    fi
-}
-
-mtime() {
-    local file="$1"
-
-    command stat -f "%m" $file
-}
-
-now() {
-
-    date "+%s"
-}
-
-p6df::modules::shell::init
